a530d06d662ca6220afcad1d6450054385b7a17a
acer <acer@acer-Aspire-One-753>
2015-05-08 02:41:17 +0300

    making design of the header

--- themes/loop_theme/views/layouts/main.php
+++ themes/loop_theme/views/layouts/main.php
@@ -30,8 +30,8 @@
         <?php  echo '<!--[if lt IE 9]>
                 <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
                 <script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js"></script>';
-                Yii::app()->getClientScript()->registerCssFile($mainAssets . '/css/ie8-and-down.css'); 
-                echo '<![endif]-->'
+                echo '<link href="'.$mainAssets .'/css/ie8-and-down.css" type="text/css" rel="stylesheet">'; 
+                echo '<![endif]-->';
         ?>
 
         <!-- Favicons
